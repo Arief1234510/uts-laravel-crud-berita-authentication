8aacd9bf82c2797f2091c978645f4265b1e0ffd2
M. Arif Setya Utama <yayajune3@gmail.com>
2026-04-22 07:09:45 +0700

    Update UserSeeder to match admin credentials

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,16 +14,16 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::create([
-            'name'     => 'Admin Argon',
-            'email'    => 'admin@example.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password123'),
+            'name'     => 'Admin ',
+            'email'    => 'admin@gmail.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('admin12345'),
             'role'     => 'admin', // Mengisi kolom role yang kita buat tadi
         ]);
     
         \App\Models\User::create([
             'name'     => 'Regular User',
             'email'    => 'user@example.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password123'),
+            'password' => \Illuminate\Support\Facades\Hash::make('admin12345'),
             'role'     => 'user',
         ]);
     }
